83a779eff08275a3c5d566e58357680c20bf7b1d
Adam Barczynski <45177740+adambar88@users.noreply.github.com>
2026-03-19 00:07:42 +0100

    Add kill-switch SW to purge stale snake service worker from browsers

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+// Kill-switch: unregisters any old service worker registered at this scope
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+            .then(() => self.clients.matchAll({ includeUncontrolled: true }))
+            .then(clients => clients.forEach(c => c.navigate(c.url)))
+            .then(() => self.registration.unregister())
+    );
+});
